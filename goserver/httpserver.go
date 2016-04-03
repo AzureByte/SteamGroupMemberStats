ebfddb3c941c943a1c425d202dd6075377cfcf45
Kenric D'Souza <AzureByte@users.noreply.github.com>
2016-04-03 07:53:55 +0530

    Feature/xml consumption and parsing go lang
    * Decided against using encoding/xml for now.
    Created functions to handle retrieval of the xml response to Steam's "memberslistxml" API and parse the response to get an array of SteamIDs.
    
    * Using first letter capital for properties.
    * Now pulling live data from steam groups.
    + Added profileStates map
    + Added the structure got from GetPlayerSummaries
    + Removed dummy data and now sending actual values
    + Added logic to sort an array map using it's values.

--- goserver/httpserver.go
+++ goserver/httpserver.go
@@ -5,8 +5,64 @@ import (
     "io/ioutil"
     "net/http"
     "html/template"
-    //"encoding/json"
+    "strings"
+    "strconv"
+    "math"
+    "encoding/json"
+    "sort"
 )
+
+
+/**************************
+
+Steam Related
+
+**************************/
+
+var (
+    profileStates = map[int]string{
+        0: "Offline",
+        1: "Online",
+        2: "Busy",
+        3: "Away",
+        4: "Snooze",
+        5: "looking to trade",
+        6: "looking to play",
+    }
+    steamAPIKey = ""
+)
+
+type JsonGetPlayerSummariesResponse struct {
+    Response Response
+}
+
+type Response struct {
+    Players []MemberInfo
+}
+
+type MemberInfo struct {
+    Steamid string
+    Personaname string
+    Personastate int // 0 - Offline, 1 - Online, 2 - Busy, 3 - Away, 4 - Snooze, 5 - looking to trade, 6 - looking to play.
+    Gameid string
+    Gameextrainfo string
+    Communityvisibilitystate int // 1 - the profile is not visible to you (Private, Friends Only, etc), 3 - the profile is "Public"
+}
+
+type AllGames struct {
+    TopGames []Game
+}
+
+type Game struct {
+    Name string
+    Gameid string
+    NumberOfPlayers int
+}
+
+
+/*************************/
+
+
 
 type Page struct {
     Title string
@@ -32,23 +88,64 @@ func handler(r_writer http.ResponseWriter, req *http.Request) {
 
 func currentlyPlayingHandler (r_writer http.ResponseWriter, req *http.Request) {
     communityid := req.URL.Path[len("/community/"):]
+    gamecounts := make(map[string]int)
+    gamenames := make(map[string]string)
 
-    //Get data from SteamAPI here.
+    // Get data from SteamAPI
+    memberslistxml := getMembersListXML(communityid)
+    SteamIDs := getSteamIdsFromXmlResponse(string(memberslistxml));
+
+    NumberOfIDs := len(SteamIDs)
+    NumberOfRequests := int(math.Ceil(float64(NumberOfIDs)/100))
+
+    var r JsonGetPlayerSummariesResponse
+    // Create a string for each set of max of n members
+    n := 100
+    i := 0
+    for i < NumberOfRequests - 1 {
+        s := strings.Join(SteamIDs[i*n : i*n + n], ",")
+
+        r = getPlayerSummariesJSON(s, steamAPIKey)
+        for _,v := range r.Response.Players {
+            if v.Communityvisibilitystate == 3 && v.Gameid != "" {
+                gamecounts[v.Gameid] += 1
+                gamenames[v.Gameid] = v.Gameextrainfo
+            }
+        }
+
+        i++
+    }
+    //s := strings.Join(SteamIDs[i:], ",")
+
+    // r = getPlayerSummariesJSON(s, steamAPIKey)
+    // for _,v := range r.Response.Players {
+    //     if v.Communityvisibilitystate == 3 && v.Gameid != "" {
+    //         gamecounts[v.Gameid] += 1
+    //         gamenames[v.Gameid] = v.Gameextrainfo
+    //     }
+    // }
 
 
-    const data = `{ "topgames" : [
-    { "name" : "Dota 2",                           "gameid" : "123321", "numberOfPlayers": 932474},
-    { "name" : "Counter-Strike: Global Offensive", "gameid" : "123322", "numberOfPlayers": 297220},
-    { "name" : "Team Fortress 2",                  "gameid" : "123323", "numberOfPlayers": 54002},
-    { "name" : "Tom Clancy's The Division",        "gameid" : "123324", "numberOfPlayers": 50007},
-    { "name" : "Grand Theft Auto V",               "gameid" : "123325", "numberOfPlayers": 42798},
-    { "name" : "Warframe",                         "gameid" : "123326", "numberOfPlayers": 31640},
-    { "name" : "Sid Meier's Civilization V",       "gameid" : "123327", "numberOfPlayers": 29243},
-    { "name" : "Football Manager 2016",            "gameid" : "123328", "numberOfPlayers": 28598},
-    { "name" : "Fallout 4",                        "gameid" : "123329", "numberOfPlayers": 27485},
-    { "name" : "ARK: Survival Evolved",            "gameid" : "123330", "numberOfPlayers": 27370}]}`;
 
-    fmt.Println(data);
+    var topg AllGames
+    sorted := getSortedKeys(gamecounts)
+
+    for _, v := range sorted[0:int(math.Min(float64(len(sorted)), 10))] {
+        var g Game
+        g.Name = gamenames[v]
+        g.Gameid = v
+        g.NumberOfPlayers = gamecounts[v]
+        topg.TopGames = append(topg.TopGames, g)
+    }
+
+    jbytes, err := json.Marshal(topg)
+    if err != nil {
+        panic(err)
+    }
+
+    data := string(jbytes)
+
+    fmt.Println(data)
 
     pg, err := loadPage(communityid)
     if err != nil {
@@ -67,8 +164,114 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+
+
 func main() {
     http.HandleFunc("/", handler)
     http.HandleFunc("/community/", currentlyPlayingHandler)
     http.ListenAndServe(":8888", nil)
 }
+
+
+
+func getPlayerSummariesJSON(csvSteamIDs string, APIKey string) JsonGetPlayerSummariesResponse {
+
+    var r JsonGetPlayerSummariesResponse
+    url := "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + APIKey + "&steamids=" + csvSteamIDs
+    fmt.Println(url)
+    resp, err_Get := http.Get(url)
+    if err_Get != nil {
+        panic(err_Get)
+    }
+
+    playersummariesjsonbytes, err_ReadAll := ioutil.ReadAll(resp.Body)
+    if err_ReadAll != nil {
+        panic(err_ReadAll)
+    }
+
+    err_Unmarshal := json.Unmarshal(playersummariesjsonbytes,&r)
+    if err_Unmarshal != nil {
+        panic(err_Unmarshal)
+    }
+
+    return r
+}
+
+// Get group details from XML API
+func getMembersListXML(communityId string) string {
+
+    // Check whether the community id is an integer and format the request accordingly.
+    commIdAsInt, err := strconv.Atoi(communityId)
+    var url string
+
+    if err != nil && commIdAsInt > 0 {
+        url = "https://steamcommunity.com/gid/" + communityId + "/memberslistxml/?xml=1" // Note when getting data via gid, despite requesting https, Steam will return data via http
+    } else {
+        url = "https://steamcommunity.com/groups/" + communityId + "/memberslistxml/?xml=1"
+    }
+
+    resp, err := http.Get(url)
+    defer resp.Body.Close()
+
+    memberslistxmlbytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+
+    return string(memberslistxmlbytes)
+}
+
+
+// Hack to retrieve the steamIds via the XML
+func getSteamIdsFromXmlResponse(xml string) []string {
+
+    // Exlude the first chunk before the IDs
+    steamIDs := strings.SplitAfter(xml, "<steamID64>")[1:]
+
+    // Replace the xml tags in each of the elements
+    for i := 0; i < len(steamIDs); i++ {
+        steamIDs[i] = strings.Replace(steamIDs[i], "</steamID64>\r\n", "", -1)
+        steamIDs[i] = strings.Replace(steamIDs[i], "<steamID64>", "", -1)
+    }
+
+    // Replace the last part of the xml Doc
+    lastEle := steamIDs[len(steamIDs)-1]
+    steamIDs[len(steamIDs)-1] = strings.SplitN(lastEle, "</members>", 2)[0]
+
+    return steamIDs
+}
+
+/**************************
+
+Sorting Logic
+
+**************************/
+
+
+// Using the logic found here https://gist.github.com/ikbear/4038654
+type sortedMap struct {
+    orig map[string]int //original
+    srt []string //sorted
+}
+func (sm *sortedMap) Len() int {
+    return len(sm.orig)
+}
+func (sm *sortedMap) Less(i, j int) bool {
+    return sm.orig[sm.srt[i]] > sm.orig[sm.srt[j]]
+}
+func (sm *sortedMap) Swap(i, j int) {
+    sm.srt[i], sm.srt[j] = sm.srt[j], sm.srt[i]
+}
+func getSortedKeys(unsortedMap map[string]int) []string {
+    sm := new(sortedMap)
+    sm.orig = unsortedMap
+    sm.srt = make([]string, len(unsortedMap))
+    i := 0
+    for key, _ := range unsortedMap {
+        sm.srt[i] = key
+        i++
+    }
+    sort.Sort(sm)
+    return sm.srt
+}
+/*************************/
